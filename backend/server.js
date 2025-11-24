7d6e99ef8b95b38aac86a79e8da663e905f8b448
RendoslandDev <agyapongrendosland53@gmail.com>
2025-11-24 19:28:37 +0000

    added api

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://mmesa-server.vercel.app',
+    origin: process.env.FRONTEND_URL || 'http://mmesa-client.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
