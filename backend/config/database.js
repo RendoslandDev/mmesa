5cd163c44a0b199eb69a712164939009c1b7f04f
RendoslandDev <agyapongrendosland53@gmail.com>
2025-11-25 14:28:46 +0000

    change mysql to postgresql

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,17 +4,23 @@ dotenv.config();
 
 const { Pool } = pkg;
 
+
+
+
 export const pool = new Pool({
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT) || 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false, // enable SSL
-    max: 100,
+    ssl: {
+        rejectUnauthorized: false // important for Render
+    },
+    max: 20,
     idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000
+    connectionTimeoutMillis: 5000
 });
+
 
 export async function query(text, params) {
     const client = await pool.connect();
